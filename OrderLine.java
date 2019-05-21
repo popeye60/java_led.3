c0e5200bf4ca8914a895deba223fdbf2122f3df4
popeye60 <43287150+popeye60@users.noreply.github.com>
2019-05-21 15:27:26 +0700

    Add files via upload

---
+++ OrderLine.java
@@ -0,0 +1,20 @@
+
+public class OrderLine {
+	Product product;
+	int quantity;
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	
+}
